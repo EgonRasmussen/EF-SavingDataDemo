6526c2493cf86a3c58fafda6a4e9a690f4de5d89
ECR\Egon Rasmussen <ecr@live.dk>
2019-03-08 12:09:36 +0100

    Add project files.

---
+++ SavingData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SavingData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
