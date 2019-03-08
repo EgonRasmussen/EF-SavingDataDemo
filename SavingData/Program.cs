7b4c6ffd3cf050ffd1a192868bd443af3648e5bd
ECR\Egon Rasmussen <ecr@live.dk>
2019-03-08 16:44:18 +0100

    ok

--- SavingData/Program.cs
+++ SavingData/Program.cs
@@ -13,96 +13,14 @@ namespace SavingData
     {
         static void Main(string[] args)
         {
-            //AddingGraphNewEntities();
-            AddingRelatedEntity();
-            //ChangingRelationships();
-            //RemovingRelationships();
-
-            //SavingDataConnected();
             //SavingDataDisconnected();
 
             //UpdateDisconnected();
             //UpdateGraphDisconnected();
             //AttachGraphDisconnected();
-
-            //DeletePost();
         }
 
 
-
-
-
-        #region SAVING RELATED DATA
-        private static void AddingGraphNewEntities()
-        {
-            using (var context = new BloggingContext())
-            {
-                var blog = new Blog
-                {
-                    Url = "http://blogs.msdn.com/dotnet",
-                    Posts = new List<Post>
-                    {
-                        new Post { Title = "Intro to C#" },
-                        new Post { Title = "Intro to VB.NET" },
-                        new Post { Title = "Intro to F#" }
-                    }
-                };
-
-                context.Blogs.Add(blog);
-                context.SaveChanges();
-            }
-        }
-
-        private static void AddingRelatedEntity()
-        {
-            using (var context = new BloggingContext())
-            {
-                var blog = context.Blogs
-                    .Include(b => b.Posts)            // Mangler denne linje, kastes en "Object reference not set to an instance of an object" exception. 
-                    .First();
-                var post = new Post { Title = "Intro to EF Core" };
-
-                blog.Posts.Add(post);
-                //blog.Posts = new List<Post> { post };   // Alternativ til at indlæse relaterede Blogs
-                context.SaveChanges();
-            }
-        }
-
-        private static void ChangingRelationships()
-        {
-            using (var context = new BloggingContext())
-            {
-                var blog = new Blog { Url = "http://blogs.msdn.com/visualstudio" };
-                var post = context.Posts.First();
-
-                post.Blog = blog;
-                context.SaveChanges();
-            }
-        }
-
-        private static void RemovingRelationships()
-        {
-            using (var context = new BloggingContext())
-            {
-                var blog = context.Blogs.Include(b => b.Posts).First();
-                var post = blog.Posts.First();
-
-                blog.Posts.Remove(post);
-                context.SaveChanges();
-            }
-        }
-        #endregion
-
-        #region UPDATE CONNECTED/DISCONNECTED
-        private static void SavingDataConnected()
-        {
-            using (var context = new BloggingContext())
-            {
-                var blog = context.Blogs.First();
-                blog.Url = "http://sample.com/connected";
-                context.SaveChanges();
-            }
-        }
 
         private static void SavingDataDisconnected()
         {
@@ -174,21 +92,7 @@ namespace SavingData
                 contextSecond.SaveChanges();
             }
         }
-        #endregion
 
-        #region DELETE
-        private static void DeletePost()
-        {
-            using (var context = new BloggingContext())
-            {
-                var post = context.Posts
-                    .Include(p => p.Tags)
-                    .Include(pt => pt.Tags);
-                context.Posts.Remove(post.First());
-                context.SaveChanges();
-            }
-        }
-        #endregion
 
         #region DISPLAY STATES
         private static void DisplayStates(IEnumerable<EntityEntry> entries)
