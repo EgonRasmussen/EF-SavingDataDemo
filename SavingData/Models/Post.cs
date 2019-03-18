9df90c2a50a3923abecabb9861481b6ed6e1d673
ECR\Egon Rasmussen <ecr@live.dk>
2019-03-18 19:55:59 +0100

    ok

--- SavingData/Models/Post.cs
+++ SavingData/Models/Post.cs
@@ -5,7 +5,6 @@
         public int PostId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-        public int Rating { get; set; }
 
         public int BlogId { get; set; }
         public Blog Blog { get; set; }
