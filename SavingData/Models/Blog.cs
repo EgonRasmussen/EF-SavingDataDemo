17cc2fe62000c3eb30b44a3b85277a7937be71ce
ECR\EgonRasmussen <ecr@eucsyd.dk>
2021-09-21 16:27:05 +0200

    ok

--- SavingData/Models/Blog.cs
+++ SavingData/Models/Blog.cs
@@ -7,6 +7,6 @@ namespace SavingData.Models
         public int BlogId { get; set; }
         public string Url { get; set; }
 
-        public List<Post> Posts { get; set; }; // = new List<Post>();
+        public List<Post> Posts { get; set; } // = new List<Post>();
     }
 }
